59ad5608fa8c4f6e6117deffb85f8c8078edf4e4
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-06-09 12:14:49 +0530

    feat: add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
-
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
 
-// 1st approch
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed", err);
+  });
+
+// 1st approch for db connection
 /*
+import mongoose from "mongoose";
+import {DB_NAME} from './constants';
 import express from "express";
+
 const app = express()(async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// 1st approch - 
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/*
+const asyncHandler = () => {};
+const asyncHandler = (func) => () => {};
+const asyncHandler = (func) => async () => {};
+*/
+
+// 2nd approch -
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+*/
